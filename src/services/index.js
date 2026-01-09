c566e0299883d20ca099b94cfc3729befca33b9c
Aneeqkhan11 <aneeqkhan113@gmail.com>
2026-01-09 19:40:07 +0500

    updates

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // let baseUrl = 'http://192.168.0.62:4000/api/';
 // let baseUrl = 'https://0g01d8wd-4000.inc1.devtunnels.ms/api/';
-let baseUrl = 'https://sirldigital.com/zannyFoods/api/';
+let baseUrl = 'https://pure-sands-31625-2fc0c2d49903.herokuapp.com/api/';
 // let baseUrl = 'https://rt2j38bh-4000.asse.devtunnels.ms/api/';
 
 const api = async (path, params, method) => {
